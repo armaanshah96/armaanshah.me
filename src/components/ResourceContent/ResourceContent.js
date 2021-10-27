dd1d3289a640a8ac6b4ec6f72a4e0ecf134ada71
Armaan Shah <armaanshah96@gmail.com>
2021-10-27 00:25:38 -0400

    Cleanup and adding alt to resources

--- src/components/ResourceContent/ResourceContent.js
+++ src/components/ResourceContent/ResourceContent.js
@@ -1,12 +1,32 @@
-import classes from './ResourceContent.module.css';
+import classes from "./ResourceContent.module.css";
 
 const ResourceContent = () => {
-  return <div className={classes.resources}>
-    <img className={classes.resource} src={process.env.PUBLIC_URL + "/images/tree-of-life-groundskeeper.jpeg"}/>
-    <img className={classes.resource} src={process.env.PUBLIC_URL + "/images/tree-of-life-arborist.png"}/>
-    <img className={classes.resource} src={process.env.PUBLIC_URL + "/images/tree-of-life-horticulturist.png"}/>
-    <img className={classes.resource} src={process.env.PUBLIC_URL + "/images/tree-of-life-crazy-scarecrow.png"}/>
-  </div>
-}
+  return (
+    <div className={classes.resources}>
+      <img
+        className={classes.resource}
+        alt="groundskeeper character"
+        src={process.env.PUBLIC_URL + "/images/tree-of-life-groundskeeper.jpeg"}
+      />
+      <img
+        className={classes.resource}
+        alt="arborist character"
+        src={process.env.PUBLIC_URL + "/images/tree-of-life-arborist.png"}
+      />
+      <img
+        className={classes.resource}
+        alt="horticulturist character"
+        src={process.env.PUBLIC_URL + "/images/tree-of-life-horticulturist.png"}
+      />
+      <img
+        className={classes.resource}
+        alt="crazy scarecrow character"
+        src={
+          process.env.PUBLIC_URL + "/images/tree-of-life-crazy-scarecrow.png"
+        }
+      />
+    </div>
+  );
+};
 
 export default ResourceContent;
